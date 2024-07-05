ca457308882241e7ff8a79e5b72e52f83fd93268
ImKetan1610 <ketan.makode@gmail.com>
2024-07-06 05:20:26 +0530

    added few pages

--- Frontend/src/Component/Home.jsx
+++ Frontend/src/Component/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./Login/Login";
+import About from "./About/About";
 import Practice from "./Practice";
+import Chat from "./Chat/Chat";
+import Contact from "./Contact/Contact";
 
 const Home = () => {
   const appRouter = createBrowserRouter([
@@ -10,8 +13,20 @@ const Home = () => {
       element: <Login />,
     },
     {
-      path: "/practice",
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/home",
       element: <Practice />,
+    },
+    {
+      path: "/chat",
+      element: <Chat />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
     },
   ]);
   return (
